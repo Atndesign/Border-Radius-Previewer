d8e88e0de6b0afc8d755b57ec1e080ca6744c0a7
Kevin <Atnnetworkk@gmail.com>
2020-03-26 16:54:25 +0100

    add all controls

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let boxWidthInput = document.getElementById("boxWidth");
+let boxHeightInput = document.getElementById("boxHeight");
+let Lradius = document.getElementById("Lradius");
+let Tradius = document.getElementById("Tradius");
+let Dradius = document.getElementById("Dradius");
+let Bradius = document.getElementById("Bradius");
